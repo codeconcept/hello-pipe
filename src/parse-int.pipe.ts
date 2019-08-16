3e5e4bfd6162fec1ac28c2139f6a379e952ca947
codeconcept <contact@codeconcept.fr>
2019-08-16 19:14:05 +0200

    commenting

--- src/parse-int.pipe.ts
+++ src/parse-int.pipe.ts
@@ -3,6 +3,7 @@ import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    // from https://docs.nestjs.com/pipes#transformation-use-case
     const val = parseInt(value, 10);
     if (isNaN(val)) {
       throw new BadRequestException('Validation failed');
